ac21ab5b408addb56f6574d01af05404f31bcfd8
prett <prett@LAPTOP-4J8G5GMO>
2021-10-11 12:55:16 +0200

    11.10 12:54, apertura, scrittura file

--- ProgettoInfo/src/controllerCameriere/ControllerCameriere.java
+++ ProgettoInfo/src/controllerCameriere/ControllerCameriere.java
@@ -2,33 +2,34 @@ package controllerCameriere;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
 import model.Listino;
 import ViewCameriere.ViewCameriere;
 
 public class ControllerCameriere implements ActionListener {
 
-		private ViewCameriere viewCameriere;
-		private Listino listino;
-		String temp="IN PREPARAZIONE:";
-	
-		
+	private ViewCameriere viewCameriere;
+	private Listino listino;
+	String temp = "IN PREPARAZIONE:";
+
 	public ControllerCameriere(ViewCameriere viewCameriere) {
-		this.viewCameriere=viewCameriere;
+		this.viewCameriere = viewCameriere;
 		viewCameriere.registraController(this);
 	}
-	
-	
+
 	public void actionPerformed(ActionEvent arg0) {
-	
-	if(arg0.getSource()==viewCameriere.getOrdina()) {
-		if(viewCameriere.getComboBoxOrdini().getSelectedIndex() != -1) {
-			String newOrder= viewCameriere.getComboBoxOrdini().getSelectedItem().toString();
-			temp +="\n"+ newOrder;
-			viewCameriere.getTextOrdini().setText(temp);
+
+		if (arg0.getSource() == viewCameriere.getOrdina()) {
+			if (viewCameriere.getComboBoxOrdini().getSelectedIndex() != -1) {
+				String newOrder = viewCameriere.getComboBoxOrdini().getSelectedItem().toString();
+				temp += "\n" + newOrder;
+				viewCameriere.getTextOrdini().setText(temp);
+			}
+
+			else
+				JOptionPane.showMessageDialog(null, "Non hai selezionato nessuna bevanda");
 		}
-		
-		//else
-			//JOptionPane.showMessageDialog(null, "Non hai selezionato nessuna bevanda");
-}
-}
+	}
 }
--- ProgettoInfo/src/model/Model.java
+++ ProgettoInfo/src/model/Model.java
@@ -1,6 +1,43 @@
 package model;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import model.Listino;
 
 public class Model {
+	FileWriter file;
+	 BufferedReader b;
 
+
+	public Model(FileWriter file, BufferedReader b) {
+		this.file = file;
+		this.b= b;
+	}
+
+	public void apriFile() {
+		try {
+			file = new FileWriter("Comande.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	public void scriviSuFile(String a) {
+		try {
+			file.write(a);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String leggiRigaDaFile() {
+		String riga="";
+		
+		return riga;
+	}
 }
