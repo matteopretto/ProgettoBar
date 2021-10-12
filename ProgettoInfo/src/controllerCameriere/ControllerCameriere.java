78e9ccc55fe81ee99488fe7e4547284240e76b7a
prett <prett@LAPTOP-4J8G5GMO>
2021-10-12 12:56:15 +0200

    nuove modifiche

--- ProgettoInfo/src/controllerCameriere/ControllerCameriere.java
+++ ProgettoInfo/src/controllerCameriere/ControllerCameriere.java
@@ -6,28 +6,41 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 import model.Listino;
+import model.Model;
 import ViewCameriere.ViewCameriere;
+
 //ciao
 public class ControllerCameriere implements ActionListener {
 
 	private ViewCameriere viewCameriere;
 	private Listino listino;
+	private Model modello;
 	String temp = "IN PREPARAZIONE:";
-	String strFile="";
+	String strFile = "";
+	int apriFile = 0;
+	int i = 0;
 
-	public ControllerCameriere(ViewCameriere viewCameriere) {
+	public ControllerCameriere(ViewCameriere viewCameriere, Model modello, Listino listino) {
 		this.viewCameriere = viewCameriere;
 		viewCameriere.registraController(this);
+		this.modello=modello;
+		this.listino=listino;
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
 
 		if (arg0.getSource() == viewCameriere.getOrdina()) {
+			if (apriFile == 0) {
+				modello.apriFile();
+				apriFile++;
+			} else
+				;
 			if (viewCameriere.getComboBoxOrdini().getSelectedIndex() != -1) {
 				String newOrder = viewCameriere.getComboBoxOrdini().getSelectedItem().toString();
 				temp += "\n" + newOrder;
 				viewCameriere.getTextOrdini().setText(temp);
-				
+				strFile = i + " " + newOrder + " 0\n";
+				modello.scriviSuFile(strFile);
 			}
 
 			else
--- ProgettoInfo/src/mainCameriere/MainCameriere.java
+++ ProgettoInfo/src/mainCameriere/MainCameriere.java
@@ -1,9 +1,15 @@
 package mainCameriere;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 import ViewCameriere.ViewCameriere;
 import controllerCameriere.ControllerCameriere;
+import model.Model;
+import model.Listino;
+
 
 public class MainCameriere {
 	public static void main(String[] args) {
@@ -11,7 +17,16 @@ public class MainCameriere {
 			public void run() {
 				try {
 					ViewCameriere window = new ViewCameriere();
-					ControllerCameriere controller= new ControllerCameriere(window);
+					BufferedReader b=null;
+					 b= new BufferedReader(new FileReader("Comande.txt"));
+					
+					FileWriter fw= new FileWriter("Comande.txt");
+					String nome="";
+					float prezzo=0;
+					Model modello= new Model(fw, b);
+					Listino listino= new Listino(nome, prezzo);
+					
+					ControllerCameriere controller= new ControllerCameriere(window, modello, listino);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
